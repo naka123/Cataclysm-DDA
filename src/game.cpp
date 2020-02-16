41e4d3a3749ff1147444e6048aeafc7b458d2770
naka123 <naka123@kukabarra.info>
2020-02-16 09:49:34 +0600

    disable robot - spawn ammo at same z as robot itself

--- src/game.cpp
+++ src/game.cpp
@@ -9491,7 +9491,7 @@ bool game::disable_robot( const tripoint &p )
         if( !critter.has_flag( MF_INTERIOR_AMMO ) ) {
             for( std::pair<const itype_id, int> &ammodef : critter.ammo ) {
                 if( ammodef.second > 0 ) {
-                    m.spawn_item( p.xy(), ammodef.first, 1, ammodef.second, calendar::turn );
+                    m.spawn_item( p, ammodef.first, 1, ammodef.second, calendar::turn );
                 }
             }
         }         
